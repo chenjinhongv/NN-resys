cbf5f3ab7eb5ef78993570cbef885fba9935585b
huangtaogan <9470545+huangtaogan@user.noreply.gitee.com>
2022-07-14 11:57:04 +0800

    add component:FmInteraction

--- models/component.py
+++ models/component.py
@@ -8,9 +8,41 @@
 # Email      ：842960911@qq.com
 # Description：
 """
-
-from torch.nn import Module, Linear, ReLU, Sequential, Embedding, ModuleList, ModuleDict
+import torch
+from torch.nn import Module, Linear, ReLU, Sequential, Embedding, ModuleList, ModuleDict, Parameter
+from torch.nn.init import normal_
 import hashlib
+
+
+class FmInteraction(Module):
+    def __init__(self, left_feature_num, right_feature_num, factor_num):
+        """
+        the module process Factor Machine interaction between two fields features which called left features(field)
+        and right features(field) below
+        :param left_feature_num: left field's feature number. a feature's feature num = category number for category
+        faeture and 1 for continuous feature
+        :param right_feature_num: same as left_feature_num
+        :param factor_num:
+        """
+        super(FmInteraction, self).__init__()
+        self.left_feature_factors = Parameter(torch.empty(left_feature_num, factor_num))
+        normal_(self.left_feature_factors)
+        self.right_feature_factors = Parameter(torch.empty(right_feature_num, factor_num))
+        normal_(self.right_feature_factors)
+
+    def forward(self, left_ids, left_vals, right_ids, right_vals):
+        """
+        :param left_ids: non-zero feature's indexes of left field, tensor with shape of (non-zero feature num,) and dtype of interger
+        :param left_vals: non-zero feature's values of left field, tensor with shape of (1, non-zero feature num)
+        :param right_ids: same as left_ids
+        :param right_vals: same as left_vals
+        :return:
+        """
+        left_factors = self.left_feature_factors[left_ids, :]
+        right_factors = self.right_feature_factors[right_ids, :]
+        out = torch.mm(left_factors, right_factors.T) * torch.mm(left_vals.T, right_vals)
+        out = out.sum()
+        return out
 
 
 class MlpLayer(Module):
@@ -66,4 +98,11 @@ def hash_buckets(key, bucket_size):
 
 
 if __name__=="__main__":
-    print(hash_buckets(1246, bucket_size=10))
+    # print(hash_buckets(1246, bucket_size=10))
+    model = FmInteraction(left_feature_num=4, right_feature_num=6, factor_num=10)
+    left_ids = torch.tensor([0, 1, 3], dtype=torch.int64)
+    left_vals = torch.tensor([[1, .5, 1]])
+    right_ids = torch.tensor([2, 1, 5, 4], dtype=torch.int64)
+    right_vals = torch.tensor([[1, 1, .6, .7]])
+    out = model(left_ids, left_vals, right_ids, right_vals)
+    print(out)
